70033f3ef12d3df5ef289b79a686dba7a6d27569
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-05-29 15:46:42 +0300

    added constructor for Car.Type

--- src/main/java/com/example/carsharingservice/model/Car.java
+++ src/main/java/com/example/carsharingservice/model/Car.java
@@ -1,6 +1,8 @@
 package com.example.carsharingservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,7 +10,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
@@ -22,6 +23,7 @@ public class Car {
     private String model;
     @NotNull
     private String brand;
+    @Enumerated(EnumType.STRING)
     private Type type;
     @Min(value = 1)
     private Integer inventory;
@@ -29,6 +31,15 @@ public class Car {
     private BigDecimal dailyFee;
 
     public enum Type {
-        SEDAN, SUV, HATCHBACK, UNIVERSAL
+        SEDAN("Sedan"),
+        SUV("Suv"),
+        HATCHBACK("Hatchback"),
+        UNIVERSAL("Universal");
+
+        public final String value;
+
+        Type(String value) {
+            this.value = value;
+        }
     }
 }
