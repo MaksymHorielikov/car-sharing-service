1d3066d9cbb98ea40acf2058441bb97445e827eb
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-05-29 15:18:08 +0300

    added validation dependency and validation in Car

--- src/main/java/com/example/carsharingservice/model/Car.java
+++ src/main/java/com/example/carsharingservice/model/Car.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -16,10 +18,14 @@ public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String model;
+    @NotNull
     private String brand;
     private Type type;
+    @Min(value = 1)
     private Integer inventory;
+    @Min(value = 0)
     private BigDecimal dailyFee;
 
     public enum Type {
