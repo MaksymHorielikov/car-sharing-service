0530cbaa70337af03ded75dd29ccf30b2a7c8c8f
copypasterr <114933128+copypasterr@users.noreply.github.com>
2023-05-31 14:47:15 +0300

    added Payment service, controller, dto (#15)
    * added payment dto's and mapper
    
    * implemented payment system (non-tested)
    
    ---------
    
    Co-authored-by: AlexandLes <81527787+AlexandLes@users.noreply.github.com>

--- src/main/java/com/example/carsharingservice/repository/PaymentRepository.java
+++ src/main/java/com/example/carsharingservice/repository/PaymentRepository.java
@@ -2,6 +2,11 @@ package com.example.carsharingservice.repository;
 
 import com.example.carsharingservice.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Payment findByRentalId(Long rentalId);
+
+    Payment findBySessionId(String sessionId);
 }
