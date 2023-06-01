0a12c52887b4d2dfe4f9c3b3c8a0c584daa5581d
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-06-01 12:25:25 +0300

    added liqubase changes

--- src/main/resources/db/changelog/changes/create-users-table.sql
+++ src/main/resources/db/changelog/changes/create-users-table.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    `role` VARCHAR(255) NOT NULL
+    `role` VARCHAR(255) NOT NULL,
+    chat_id VARCHAR(255) NULL
 );
 
 --rollback DROP TABLE users;
