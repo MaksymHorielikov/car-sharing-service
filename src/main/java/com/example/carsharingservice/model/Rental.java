f9a63a346bb0a903b1cc7d1e127126742b51bd74
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-06-27 10:43:30 +0300

    changed relation

--- src/main/java/com/example/carsharingservice/model/Rental.java
+++ src/main/java/com/example/carsharingservice/model/Rental.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Data;
@@ -25,7 +26,7 @@ public class Rental {
     private LocalDateTime rentalDate;
     private LocalDateTime returnDate;
     private LocalDateTime actualReturnDate;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_id")
     private Car car;
     @ManyToOne(fetch = FetchType.LAZY)
