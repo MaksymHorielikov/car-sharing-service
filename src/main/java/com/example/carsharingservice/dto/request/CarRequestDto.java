2270bb4450ae07ab881972c142fe5c76fa0d5e19
KosMarch <kosmarchenko98@gmail.com>
2023-07-04 09:29:30 +0300

    added valid to Car

--- src/main/java/com/example/carsharingservice/dto/request/CarRequestDto.java
+++ src/main/java/com/example/carsharingservice/dto/request/CarRequestDto.java
@@ -1,15 +1,23 @@
 package com.example.carsharingservice.dto.request;
 
 import com.example.carsharingservice.model.Car;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CarRequestDto {
+    @NotEmpty(message = "This field can`t be empty")
     private String model;
+    @NotEmpty(message = "This field can`t be empty")
     private String brand;
+    @NotEmpty(message = "This field can`t be empty")
     private Car.Type type;
+    @NotEmpty(message = "This field can`t be empty")
+    @PositiveOrZero
     private Integer inventory;
+    @NotEmpty(message = "This field can`t be empty")
     private BigDecimal dailyFee;
 
     public CarRequestDto(String model,
